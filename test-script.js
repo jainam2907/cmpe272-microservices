018bae848e4b39d1b1daee547ec98149dbf44134
Jainam Chhatbar <jnchhatbar.jnc@gmail.com>
2024-09-04 23:56:08 -0700

    minor enhancements in test script

--- test-script.js
+++ test-script.js
@@ -1,11 +1,11 @@
 const axios = require('axios');
 
-const helloServiceURL = "http://localhost:62933/hello";
-const worldServiceURL = "http://localhost:62121/world";
+const helloServiceURL = "http://127.0.0.1:63412";
+const worldServiceURL = "http://127.0.0.1:63422";
 
 async function getHelloWorld() {
-    const helloResponse = await axios.get(helloServiceURL);
-    const worldResponse = await axios.get(worldServiceURL);
+    const helloResponse = await axios.get(`${helloServiceURL}/hello`);
+    const worldResponse = await axios.get(`${worldServiceURL}/world`);
 
     console.log(`${helloResponse.data} ${worldResponse.data}`);
 }
